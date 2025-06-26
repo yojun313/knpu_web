1affccb8b954cc8748d633050aecc2212b352aca
yojun313 <moonyojun@gmail.com>
2025-06-26 13:32:33 +0900

    update

--- homepage/server/app/libs/upload_image.py
+++ homepage/server/app/libs/upload_image.py
@@ -9,7 +9,7 @@ ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
 SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
 ACCOUNT_ID = os.getenv('ACCOUNT_ID')
 BUCKET_NAME = os.getenv('BUCKET_NAME')
-LOCAL_FOLDER = "D:/BIGMACLAB/MANAGER/Output"
+LOCAL_FOLDER = "D:/PAILAB/MANAGER/Output"
 R2_ENDPOINT = f"https://{ACCOUNT_ID}.r2.cloudflarestorage.com"
 
 # S3 클라이언트 생성
@@ -20,6 +20,7 @@ s3 = boto3.client(
     aws_secret_access_key=SECRET_ACCESS_KEY,
     region_name='auto'  # R2의 경우 region을 auto로 지정
 )
+
 
 def upload_image(filename: str) -> str:
     """
@@ -33,12 +34,14 @@ def upload_image(filename: str) -> str:
     # URL 반환
     return get_image_url(filename)
 
+
 def get_image_url(filename: str) -> str:
     """
     업로드된 파일의 퍼블릭 URL 반환
     :param filename: 업로드된 파일 이름
     """
     return f"{R2_ENDPOINT}/{BUCKET_NAME}/{filename}"
+
 
 # === 사용 예제 ===
 if __name__ == "__main__":
