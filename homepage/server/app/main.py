c12a6dd58ce4375220edfc6e008a06bcf24c68db
yojun313 <moonyojun@gmail.com>
2025-06-26 13:22:46 +0900

    update

--- homepage/server/app/main.py
+++ homepage/server/app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routes import api_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api", tags=["api"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 혹은 ["https://knpu.re.kr"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
