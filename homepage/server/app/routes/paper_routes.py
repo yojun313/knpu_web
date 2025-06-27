29411c113453556cfa11ae1aad7d6ea4299a7169
yojun313 <moonyojun@gmail.com>
2025-06-27 12:25:30 +0900

    update

--- homepage/server/app/routes/paper_routes.py
+++ homepage/server/app/routes/paper_routes.py
@@ -8,4 +8,9 @@ def list_papers():
     docs = list(papers_db.find())  # 모든 문서 가져오기
     for d in docs:
         d.pop("_id", None)  # Mongo의 ObjectId 제거
-    return docs  # 연도별 하나의 문서가 담긴 리스트 반환
+
+    # 연도 기준 내림차순 정렬
+    docs.sort(key=lambda x: int(x.get("year", 0)), reverse=True)
+
+    return docs
+
