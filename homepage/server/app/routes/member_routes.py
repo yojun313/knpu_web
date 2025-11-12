1ed83daa8a1eae248bead61a66ce29480bba168d
yojun313 <moonyojun@gmail.com>
2025-11-13 01:06:43 +0900

    uipdate

--- homepage/server/app/routes/member_routes.py
+++ homepage/server/app/routes/member_routes.py
@@ -12,9 +12,17 @@ def list_members():
         d["_id"] = str(d["_id"])
 
     # 정렬 기준 정의
-    order = ["교수", "수석연구위원", "연구위원", "대학원 과정", "연구원", "선임연구원"]
-    order_map = {section: idx for idx, section in enumerate(order)}
+    section_order = ["교수", "수석연구위원", "연구위원", "대학원 과정", "연구원", "선임연구원"]
+    section_order_map = {s: i for i, s in enumerate(section_order)}
 
-    # 정렬
-    docs.sort(key=lambda d: order_map.get(d.get('section'), len(order_map)))
+    position_order = ["박사과정", "석사과정", "선임연구원", "연구원"]
+    position_order_map = {p: i for i, p in enumerate(position_order)}
+
+    docs.sort(
+        key=lambda d: (
+            section_order_map.get(d.get("section"), len(section_order_map)),
+            position_order_map.get(d.get("position"), len(position_order_map))
+        )
+    )
+    
     return docs
