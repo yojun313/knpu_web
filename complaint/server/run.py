51cb99e39deb86130916c2b6ac1b3cceef747d9c
yojun313 <moonyojun@gmail.com>
2025-12-23 17:59:27 +0900

    Update complaint server port to 8003 in run.py and add entry in port.txt

--- complaint/server/run.py
+++ complaint/server/run.py
@@ -2,4 +2,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True, timeout_keep_alive=86400)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8003, reload=True, timeout_keep_alive=86400)
