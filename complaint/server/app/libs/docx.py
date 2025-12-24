444862184e85051d5df63a2ad49f87d221ec5289
yojun313 <moonyojun@gmail.com>
2025-12-25 04:47:22 +0900

    Update llm_data keys in document generation for consistency and clarity

--- complaint/server/app/libs/docx.py
+++ complaint/server/app/libs/docx.py
@@ -61,12 +61,12 @@ def word_generate(llm_data, form_data):
                 elif para.text == "피고소인 이메일":
                     para.text = form_data["피고소인 이메일"]
                 elif para.text == "피고소인 기타사항":
-                    para.text = llm_data["피고소인 기타사항"]
+                    para.text = llm_data["1"]
 
-    add_sentence(document, "(죄명 및 피고소인에 대한 처벌의사 기재)", "\n"+llm_data["고소 취지"]) # \n 무조건 추가 필요
-    add_sentence(document, "4. 범죄사실*", "\n"+llm_data["범죄 사실"]) # \n 무조건 추가 필요
-    add_sentence(document, "5. 고소이유", "\n"+llm_data["고소 이유"]) # \n 무조건 추가 필요
-    add_sentence(document, "6. 증거자료", "\n"+llm_data["증거 자료"]) # \n 무조건 추가 필요
+    add_sentence(document, "(죄명 및 피고소인에 대한 처벌의사 기재)", "\n"+llm_data["2"]) # \n 무조건 추가 필요
+    add_sentence(document, "4. 범죄사실*", "\n"+llm_data["3"]) # \n 무조건 추가 필요
+    add_sentence(document, "5. 고소이유", "\n"+llm_data["4"]) # \n 무조건 추가 필요
+    add_sentence(document, "6. 증거자료", "\n"+llm_data["5"]) # \n 무조건 추가 필요
 
     checklist_table = document.tables[2]
 
@@ -79,7 +79,7 @@ def word_generate(llm_data, form_data):
                     para.add_run(form_data["관련 형사사건 수사 유무"])
 
     formatted_date = format_korean_date(form_data["고소일자"])
-    add_sentence(document, "8. 기타", "\n"+llm_data["기타"]) # \n 무조건 추가 필요
+    add_sentence(document, "8. 기타", "\n"+llm_data["6"]) # \n 무조건 추가 필요
     add_sentence(document, "무고죄로 처벌받을 것임을 서약합니다.", "\n                                                 "+formatted_date)
     add_sentence(document, "고소대리의 경우에는 제출인을 기재하여야 합니다.", "\n\n                                                   "+f"{form_data["제출 경찰서"]} 귀중")
     suedocument_name = "AI 고소장_"+str(form_data["고소인"])+"_"+str(form_data["고소 죄명"])+"_"+str(now.strftime("%H%M%S"))+".docx"
