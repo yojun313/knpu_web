7fcfcd870a0b5aeaf801946e8a19ae657bdbf3b1
yojun313 <moonyojun@gmail.com>
2026-04-18 11:36:56 +0900

    Refactor server run configuration to use uvicorn and handle warnings

--- complaint/server/run.py
+++ complaint/server/run.py
@@ -1,10 +1,18 @@
-# gunicorn app.main:app -c run.py
 # uvicorn app.main:app --host 0.0.0.0 --port 8003
 
-bind = "0.0.0.0:8003"
-workers = 1
-worker_class = "uvicorn.workers.UvicornWorker"
-timeout = 0
-loglevel = "warning"
-accesslog = None          
-keepalive = 86400
+import uvicorn
+import warnings
+from requests.exceptions import RequestsDependencyWarning
+
+warnings.filterwarnings("ignore", category=RequestsDependencyWarning)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8003,
+        workers=1,
+        log_level="warning",
+        access_log=False,
+        timeout_keep_alive=86400
+    )
